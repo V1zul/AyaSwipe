1735ed5b03a1b0520722016a2fb22ccf76905dad
omar huseinspahic <omarhus1020@hotmail.com>
2025-09-24 21:43:14 -0400

    Offline App

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'ayaswipe-v1';
+const ASSETS = [
+  '/', '/index.html', '/style.css', '/script.js',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png', '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e=>{
+  const { request } = e;
+  if (request.url.includes('api.quran.com') || request.url.match(/\.mp3($|\?)/)) {
+    e.respondWith(fetch(request).catch(()=>caches.match(request)));
+  } else {
+    e.respondWith(caches.match(request).then(r=>r||fetch(request)));
+  }
+});
